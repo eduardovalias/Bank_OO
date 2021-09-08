e4080293954fb6b39c92b3d47c2bf577d9c42d05
Eduardo Valias <edupv123@hotmail.com>
2021-09-08 09:51:14 -0300

    Adicionar arquivos do banco

---
+++ myBank/Client.cs
@@ -0,0 +1,6 @@
+public class Client {
+    public string titular;
+    public int agencia;
+    public int conta;
+    public double saldo;
+}
---
+++ myBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client eduardo = new Client();
+            Client thulio = new Client();
+
+            eduardo.titular = "Eduardo";
+            eduardo.agencia = 1234;
+            eduardo.conta = 890;
+            eduardo.saldo = 333.33;
+
+            thulio.titular = "Thulio";
+            thulio.agencia = 1234;
+            thulio.conta = 900;
+            thulio.saldo = 3333333.33;
+
+            Console.WriteLine("O titular da conta é: " + eduardo.titular);
+            Console.WriteLine("O titular da conta é: " + thulio.titular);
+        }
+    }
+}
