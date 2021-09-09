023a3d48e7ed2875249809f65fdc9fbb95e0c74e
Eduardo Valias <edupv123@hotmail.com>
2021-09-09 09:43:16 -0300

    Alterar o construtor

--- myBank/Client.cs
+++ myBank/Client.cs
@@ -3,4 +3,11 @@ public class Client {
     public int agencia;
     public int conta;
     public double saldo;
+
+    public Client(string titular, int agencia, int conta, double saldo) {
+        this.titular = titular;
+        this.agencia = agencia;
+        this.conta = conta;
+        this.saldo = saldo;
+    }
 }
--- myBank/Program.cs
+++ myBank/Program.cs
@@ -6,21 +6,13 @@ namespace myBank
     {
         static void Main(string[] args)
         {
-            Client eduardo = new Client();
-            Client thulio = new Client();
-
-            eduardo.titular = "Eduardo";
-            eduardo.agencia = 1234;
-            eduardo.conta = 890;
-            eduardo.saldo = 333.33;
-
-            thulio.titular = "Thulio";
-            thulio.agencia = 1234;
-            thulio.conta = 900;
-            thulio.saldo = 3333333.33;
+            Client eduardo = new Client("Eduardo", 1234, 890, 333.33);
+            Client thulio = new Client("Thulio", 1234, 900, 3333333.33);
+            Client cana = new Client("Cana", 1234, 999, 3333333333333333333.33);
 
             Console.WriteLine("O titular da conta é: " + eduardo.titular);
             Console.WriteLine("O titular da conta é: " + thulio.titular);
+            Console.WriteLine("O titular da conte é: " + cana.titular);
         }
     }
 }
